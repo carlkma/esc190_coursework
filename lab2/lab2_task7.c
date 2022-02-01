7b594354828933a9e9369f963132132b0ed52638
macarl08 <macarl@outlook.com>
2022-02-01 13:31:41 -0500

    lab2_done

--- lab2/lab2_task7.c
+++ lab2/lab2_task7.c
@@ -1,12 +1,39 @@
-#include <stdio.h>
+//#include <stdio.h>
 #include <stdlib.h>
 
 
+void append(int** x, int* n, int val){
 
+	*n = *n + 1;
+	//printf("%d\n", *n);
+	*x = (int*) realloc(*x, sizeof(int) * (*n));
 
-int main(){
-	int x = 0;
-	int* ptr = &x;
-	fprintf(stdout, "x contains the value %d at location %p\n", *ptr, ptr);
 	
+	(*x)[*n - 1] = val;
+
+	//printf("%d\n", *(*x+0));
+	//printf("%d\n", *(*x+1));
+	//printf("%d\n", *(*x+2));
+
+	
+
 }
+/*
+int main(){
+	int size = 2;
+	int* n = &size;
+	int* arr = (int*) malloc( sizeof (int) * *n);
+	
+
+	*arr = 1;
+	*(arr+1) = 2;
+	//*(arr+2) = 3;
+
+	int** x = &arr;
+	append(x,n,50);
+
+	int* the_new_arr = *x;
+
+	printf("%d and %d and %d and %d and %d", *(the_new_arr), *(the_new_arr+1),*(the_new_arr+2),*(the_new_arr+3),*(the_new_arr+4));
+}
+*/
