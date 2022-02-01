2e109ad0753f87339596bb04a55727eeb240c618
macarl08 <macarl@outlook.com>
2022-02-01 12:04:18 -0500

    task6

--- lab2/lab2_task5.c
+++ lab2/lab2_task5.c
@@ -10,18 +10,21 @@ int main(){
 	int *q = (int *) malloc ( sizeof (int) * 2);
 	*p = 1;
 	*q = 10;
+	/*
 	printf("Before\n");
 	printf("%d and %p\n",*p,p);
 	printf("%d and %p\n",*(p+1),(p+1));
 	printf("%d and %p\n",*q,q);
 	printf("%d and %p\n",*(q+1),(q+1));
+	*/
 	q = p;
-
+	/*
 	printf("After\n");
 	printf("%d and %p\n",*p,p);
 	printf("%d and %p\n",*(p+1),(p+1));
 	printf("%d and %p\n",*q,q);
 	printf("%d and %p\n",*(q+1),(q+1));
+	*/
 
 	free (q);
 	free (p);
--- lab2/lab2_task6.c
+++ lab2/lab2_task6.c
@@ -5,14 +5,37 @@
 int main(){
 	
 
-	int ** p;
+	int** p;
 	int x = 10;
 	int y = 11;
 	int z = 12;
-	int * ptr = (int *) malloc ( sizeof (int) * 3);
+	int* ptr = (int*) malloc( sizeof (int) * 3);
 	*ptr = x;
-	*( ptr + 1) = y;
-	*( ptr + 2) = z;
+	*(ptr+1) = y;
+	*(ptr+2) = z;
+
+	/*
+	printf("xyz\n");
+	printf("%d and %p\n",x,&x);
+	printf("%d and %p\n",y,&y);
+	printf("%d and %p\n",z,&z);
+
+	printf("Before\n");
+	printf("%d and %p\n",*(ptr),ptr);
+	printf("%d and %p\n",*(ptr+1),(ptr+1));
+	printf("%d and %p\n",*(ptr+2),(ptr+2));
+	*/
+
 	p = &ptr;
-	
+
+	/*
+	printf("After\n");
+	printf("%d and %p and %p\n",**p, *p,p);
+	printf("%d and %p and %p\n",*(*(p)+1), *(p)+1,(p));
+	printf("Done!\n");
+	*/
+
+	free(ptr);
+	free(p);
+
 }
