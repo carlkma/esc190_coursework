7220f608ca39d788f67125bbee0f477a20e61ad7
macarl08 <macarl@outlook.com>
2022-03-10 18:53:11 -0500

    add comments

--- a1/a1.c
+++ a1/a1.c
@@ -1,24 +1,34 @@
-#include "a1.h"
+/* --------------------------------------------------------------------------------
 
+// ESC190: Computer Algorithms and Data Structures
+// Assignment 1
+// by Carl Ma and Shenxiaozhu Xu
+// March 30, 2022
+
+-------------------------------------------------------------------------------- */
+
+#include "a1.h" // header file provided with the assignment, unmodified
+
+// "precision (of the double item_cost_per_unit) is not guaranteed"
+// hence, the standard maximum precision of a double type is assumed
 #define MAX_PRECISION_DOUBLE 16
 
-/**
-	Add your functions to this file.
-	Make sure to review a1.h.
-	Do NOT include a main() function in this file
-	when you submit.
-*/
 
-
-int a_isspace(unsigned char c){
+// helper function to determine if char c is a whitespace character
+// input: unsigned char c
+// output: boolean (int) 0 or 1
+int a_isspace(unsigned char c) {
 	const char whitespace[] = " \n\t\v\r";
-
-	if (strchr(whitespace, c) == NULL) return 0;
+	if (strchr(whitespace, c) == NULL) return 0; // is c not in "whitespace"?
 	return 1;
 }
 
+
+// helper function to trim leading and trailing whitespaces
+// input: string str - a pointer to char
+// output: string new_str - a pointer to char
+// note: this function is adapted from stackoverflow
 char *trimwhitespace(char *str) {
-// this function is adapted from stackoverflow
   char *end;
 
   // Trim leading space
@@ -34,6 +44,17 @@ char *trimwhitespace(char *str) {
   return str;
 }
 
+
+/* --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+FIRST TASK: Initialize Restaurant
+
+Memory Allocation Records (net direct +3)
+ + Restaurant
+ + Restaurant->name (char*)
+ + Restaurant->pending_order (Queue*)
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------- */
 Restaurant* initialize_restaurant(char* name) {
 
 	// Initialize restaurant
@@ -57,68 +78,91 @@ Restaurant* initialize_restaurant(char* name) {
 	new_queue->head = NULL;
 	new_queue->tail = NULL;
 	new_restaurant->pending_orders = new_queue;
-
 }
+
+
+/* --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+2nd TASK: Load Menu
+
+Memory Allocation Records (net direct +4, with loop +2)
+ + Menu
+ + line (char*) once
+ + line (char*) twice
+ + Menu->item_codes (char**)
+ +  			   -> code (for every idx) (char*) 
+ + Menu->item_names (char**)
+ +  			   -> name (for every idx) (char*) 
+ + Menu->item_cost_per_unit (double*)
+
+ - line (char*) once
+ - line (char*) twice
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------- */
 Menu* load_menu(char* fname) {
 
 	// Initialize menu
 	Menu *new_menu;
 	new_menu = (Menu *) malloc(sizeof(Menu));
-
-	// Initialize menu attributes
-	int count_items = 0;
+	
 	
 	// Get number of items
+	int count_items = 0;
+
+	// Open file stream
 	FILE *input_file;
 	input_file = fopen(fname, "r");
-	char *line;
+	
+	// Define whitespace characters
 	const char whitespace[] = " \n\t\v\r";
 
+	// Allocate a temp space for each line read from file
+	char *line;
 	line = (char *) malloc(ITEM_CODE_LENGTH + MAX_ITEM_NAME_LENGTH + MAX_PRECISION_DOUBLE);
+
+	// Looping through each line from file
+	// Incrementing count_item only if the line is not full of whitespace
 	while (fgets(line, ITEM_CODE_LENGTH + MAX_ITEM_NAME_LENGTH + MAX_PRECISION_DOUBLE, input_file) != NULL) {
 		if (strspn(line,whitespace) != strlen(line)) count_items++;
 	}
 	
+	// Close file and free temp allocation
 	fclose(input_file);
 	free(line);
 
-	// Continue to initialize menu attributes
+	// Initialize menu attributes
 	char **item_codes_array;
 	item_codes_array = (char **) malloc(sizeof(char*) * count_items);
 	char **item_names_array;
 	item_names_array = (char **) malloc(sizeof(char*) * count_items);
 	double *item_cost_array = (double *) calloc(sizeof(double) , count_items);
 
+	// Allocate a temp space for each line read from file again
 	// Read file line by line
 	line = (char *) malloc(ITEM_CODE_LENGTH + MAX_ITEM_NAME_LENGTH + MAX_PRECISION_DOUBLE);
 	
-	int idx = 0;
-	input_file = fopen(fname, "r");
+	int idx = 0; // Temp index variable for populating array
+	input_file = fopen(fname, "r"); // Open file stream again
 
 	while (fgets(line, ITEM_CODE_LENGTH + MAX_ITEM_NAME_LENGTH + MAX_PRECISION_DOUBLE, input_file) != NULL) {
 		
-
+		// Split line by delimiter MENU_DELIM
 		char *item_code = strtok(line, MENU_DELIM);
 		char *item_name = strtok(NULL, MENU_DELIM);
 		char *item_cost = strtok(NULL, MENU_DELIM);
 		
-
+		// Utilization of the helper function trimwhitespace()
+		// in order to strip leading and trailing whitespace
 		item_codes_array[idx] = (char *) calloc(sizeof(char) , ITEM_CODE_LENGTH);
 		strcpy(item_codes_array[idx], trimwhitespace(item_code));
 		
-
 		item_names_array[idx] = (char *) calloc(sizeof(char) , MAX_ITEM_NAME_LENGTH);
 		strcpy(item_names_array[idx], item_name);
 		
-
-		char *item_cost_clean = item_cost+1; // removes first character
-  		//item_cost_clean[strlen(item_cost)-1] = '\0'; // removes last character
-		
-		//item_cost_array[idx] = strtod(item_cost+1, NULL);
-		item_cost_array[idx] = strtod(item_cost_clean, NULL);
+		item_cost_array[idx] = strtod(item_cost+1, NULL); // +1 to skip dollar sign
 		
 		idx++;
-		if (idx==count_items) break;
+		if (idx==count_items) break; // once all non-empty lines are processed, loop terminates
 	}
 
 	fclose(input_file);
@@ -129,49 +173,63 @@ Menu* load_menu(char* fname) {
 	new_menu->item_cost_per_unit = item_cost_array;
 
 	return new_menu;
-
 }
 
+
+/* --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+3rd TASK: Build Order
+
+Memory Allocation Records (net direct +3, with loop +1)
+ + Order
+ + Order->item_codes (char**)
+ +                  -> code (for every idx) (char*) 
+ + Order->item_quantities (int*)
+ + cpy_quantities (char*)
+
+ - cpy_quantities
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------- */
 Order* build_order(char* items, char* quantities) {
+
+	// Initialize order
 	Order *new_order;
 	new_order = (Order *) malloc(sizeof(Order));
 
+	// Get the number of items from string items
 	int num_unique_item = strlen(items) / (ITEM_CODE_LENGTH - 1);
    
-
+	// Array for item_codes
 	char **item_codes_array_for_order;
 	item_codes_array_for_order = (char **) malloc(sizeof(char*) * num_unique_item);
+
+	// Array for item quantities
 	int *item_q_array = (int *) malloc(sizeof(int) * num_unique_item);
 
 	int code_idx = 0;
 
+	// Allocate new variable and space for quantities
+	// Otherwise the parameter is a read-only string literal that does not support manipulation
     char *cpy_quantities;
     cpy_quantities = (char *) calloc(sizeof(char), strlen(quantities)+1);
     strcpy(cpy_quantities,quantities);
 
-
+	// Loop through each of the unique items
 	for (int loop=0; loop<num_unique_item; loop++) {
 
 		// Assign item code
 		item_codes_array_for_order[loop] = (char *) calloc(sizeof(char) , (ITEM_CODE_LENGTH));
 		strncpy(item_codes_array_for_order[loop], items, ITEM_CODE_LENGTH-1);
 		items = items + ITEM_CODE_LENGTH-1;
-        //printf("%s\n",item_codes_array_for_order[loop]);
 
         // Assign item quantities
 		char *item_quantity;
-		if (loop==0) {
-			item_quantity = strtok(cpy_quantities, MENU_DELIM);
-		}
-		else {
-			item_quantity = strtok(NULL, MENU_DELIM);
-		}
+		if (loop==0) item_quantity = strtok(cpy_quantities, MENU_DELIM);
+		else item_quantity = strtok(NULL, MENU_DELIM);
         
 		item_q_array[loop] = atoi(item_quantity);
-        //printf("%d\n",item_q_array[loop]);
-
-
 	}
+
     free(cpy_quantities);
 	new_order->num_items = num_unique_item;
 	new_order->item_codes = item_codes_array_for_order;
@@ -180,6 +238,13 @@ Order* build_order(char* items, char* quantities) {
 	
 }
 
+
+/* --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+4th TASK: Enqueue
+
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------- */
 void enqueue_order(Order* order, Restaurant* restaurant) {
 
 	Queue *alias_to_restaurant_queue;
@@ -191,20 +256,25 @@ void enqueue_order(Order* order, Restaurant* restaurant) {
     
     new_queueNode->next = NULL;
 
-    
     if (alias_to_restaurant_queue->tail == NULL) {
         alias_to_restaurant_queue->tail = new_queueNode;
         alias_to_restaurant_queue->head = new_queueNode;
     }
     else {
 	alias_to_restaurant_queue->tail->next = new_queueNode;
-    
 	alias_to_restaurant_queue->tail = new_queueNode;
-    
     }
+
     restaurant->num_pending_orders = restaurant->num_pending_orders + 1;
 }
 
+
+/* --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+5th TASK: Dequeue
+
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------- */
 Order* dequeue_order(Restaurant* restaurant) {
 	Queue *alias_to_restaurant_queue;
 	alias_to_restaurant_queue = restaurant->pending_orders;
@@ -228,19 +298,17 @@ Order* dequeue_order(Restaurant* restaurant) {
 }
 
 
+
 double get_item_cost(char* item_code, Menu* menu) {
-	
 	int i;
 	for (i=0; i<(menu->num_items); i++){
 		if (strcmp(item_code, (menu->item_codes)[i]) == 0) break;
 	}
 	return (menu->item_cost_per_unit)[i];
-
 }
 
 
 double get_order_subtotal(Order* order, Menu* menu) {
-
 	double subtotal = 0;
 	int i;
 	for (i=0; i<(order->num_items); i++){
@@ -248,7 +316,6 @@ double get_order_subtotal(Order* order, Menu* menu) {
 		int this_item_quantity = (order->item_quantities)[i];
 		subtotal += get_item_cost(this_item_code, menu) * this_item_quantity;
 	}
-
 	return subtotal;
 }
 
@@ -265,6 +332,19 @@ int get_num_pending_orders(Restaurant* restaurant) {
 	return restaurant->num_pending_orders;
 }
 
+/* --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+CLEANING TASK: Clear Order
+
+Memory Allocation Records (net direct -3, with loop -1)
+ - Order
+ - Order->item_codes (char**)
+ -                  -> code (for every idx) (char*) 
+ - Order->item_quantities (int*)
+
+ ORDER IS ALL CLEARED
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------- */
 void clear_order(Order** order) {
 	for (int i=0; i<(*order)->num_items; i++){
 		free(((*order)->item_codes)[i]);
@@ -275,13 +355,28 @@ void clear_order(Order** order) {
 	*order = NULL;
 }
 
-void clear_menu(Menu** menu) {
 
+/* --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+CLEANING TASK: Clear Menu
+
+Memory Allocation Records (net direct -4, with loop -2)
+ - Menu
+ - Menu->item_codes (char**)
+ -  			   -> code (for every idx) (char*) 
+ - Menu->item_names (char**)
+ -  			   -> name (for every idx) (char*) 
+ - Menu->item_cost_per_unit (double*)
+ MENU IS ALL CLEARED
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------- */
+void clear_menu(Menu** menu) {
 
 	for (int i=0; i<(*menu)->num_items; i++){
 		free(((*menu)->item_codes)[i]);
 		free(((*menu)->item_names)[i]);
 	}
+
 	free((*menu)->item_codes);
 	free((*menu)->item_names);
 	free((*menu)->item_cost_per_unit);
@@ -290,6 +385,24 @@ void clear_menu(Menu** menu) {
 }
 
 
+/* --------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+CLEANING TASK: Close Restaurant
+
+Memory Allocation Records (net direct -3)
+ - Restaurant
+ - Restaurant->name (char*)
+ - Restaurant->pending_order (Queue*)
+ -                          -> head
+ -                          -> tail
+ - Order (every order)
+		- Order->item_codes (char**)
+		-                  -> code (for every idx) (char*) 
+		- Order->item_quantities (int*)
+
+ MENU IS ALL CLEARED
+--------------------------------------------------------------------------------
+-------------------------------------------------------------------------------- */
 void close_restaurant(Restaurant** restaurant){
 
 	clear_menu(&((*restaurant)->menu));
@@ -357,11 +470,10 @@ void print_receipt(Order* order, Menu* menu){
 
 
 /*
+
 int main() {
-	Menu * menu = load_menu ("menu.txt");
-	
+	Menu *menu = load_menu("menu.txt");
 	print_menu(menu);
-    
 }
 
 */
